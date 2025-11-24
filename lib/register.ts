890f739f11656e6863c430ccce5f07801071d2a9
Zeyln <ragnar.lintoft@gmail.com>
2025-11-24 09:33:40 +0100

    added register typeScript file

---
+++ lib/register.ts
@@ -0,0 +1,20 @@
+'use server'
+
+import { supabase } from '@/lib/supabase'
+
+export async function registerUser(user_name: string, email: string, password: string) {
+   if (!user_name || !email || !password) {
+      throw new Error('Missing user_name, email, or password')
+   }
+
+   const { data, error } = await supabase
+      .from('Users')
+      .insert([{ user_name, email, password }])
+      .select()
+
+   if (error) {
+      throw new Error(error.message)
+   }
+
+   return { message: 'User added successfully', user: data } 
+}
