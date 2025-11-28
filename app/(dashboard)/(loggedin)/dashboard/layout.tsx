48f4bc20ccc2fecb052fdbe4e73672cc40459c8a
Kristoffer1122 <krkja001@osloskolen.no>
2025-11-28 13:23:29 +0100

    more schools, slight style change

--- app/(dashboard)/(loggedin)/dashboard/layout.tsx
+++ app/(dashboard)/(loggedin)/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default function LoggedInLayout({
 
    return (
       <div className="min-h-screen">
-         <main className="container mx-auto px-4 py-8">
+         <main className="container px-4 py-8">
             {children}
          </main>
       </div>
